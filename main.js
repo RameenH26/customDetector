9678b68a4a3554979573aa6ad519c464435adbd3
Rameen Humayun <76857653+RameenH26@users.noreply.github.com>
2021-09-03 02:59:55 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+status="";
 function preload() {
 }
 function setup() {
@@ -9,4 +10,15 @@ function setup() {
 function draw() {
     image(video, 0, 0, 380, 300);
 }
-
+input = " ";
+function start() {
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById('status').innerHTML = "Status : Detecting"; 
+    input = document.getElementById('name').value;
+    console.log(input);
+}
+function modelLoaded() {
+    console.log("model loaded");
+    console.log("loaded cocossd");
+    status = true;
+}
